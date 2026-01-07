b4555f19876a738f436bbf2929ad2d8737ce246c
Anatoly Starostin <anatoli.starostin@gmail.com>
2026-01-07 21:06:33 +0200

    LUT in progress

--- src/spiky/lut/tests/test_lut_transformer_product.py
+++ src/spiky/lut/tests/test_lut_transformer_product.py
@@ -17,8 +17,8 @@ def test_lut_transformer_product(
     for g_type in [GradientType.Dense]:  # , GradientType.Sparse, GradientType.Internal
         if g_type == GradientType.Internal and summation_dtype == torch.int32:
             continue
-        for fully_connected in [True]:  # , False
-            for train_or_eval in ['eval', 'train']:  # , 'train'
+        for fully_connected in [True, False]:
+            for train_or_eval in ['eval', 'train']:
                 for batch_size in [1, 4]:
                     for sliced_mode in [False]:  # , True
                         if not sliced_mode and not fully_connected:
