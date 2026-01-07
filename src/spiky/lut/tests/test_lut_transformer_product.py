010e877afe6d92b40841716213d35b0ecce34ff2
Anatoly Starostin <anatoli.starostin@gmail.com>
2026-01-07 20:01:58 +0200

    LUT in progress

--- src/spiky/lut/tests/test_lut_transformer_product.py
+++ src/spiky/lut/tests/test_lut_transformer_product.py
@@ -14,10 +14,10 @@ from spiky.util.text_snippet_sampler import TextSnippetSampler
 def test_lut_transformer_product(
     device, summation_dtype, seed=None
 ):
-    for g_type in [GradientType.Dense, GradientType.Sparse, GradientType.Internal]:
+    for g_type in [GradientType.Dense]:   # , GradientType.Sparse, GradientType.Internal
         if g_type == GradientType.Internal and summation_dtype == torch.int32:
             continue
-        for fully_connected in [True, False]:
+        for fully_connected in [True]:  # , False
             for train_or_eval in ['eval', 'train']:
                 for batch_size in [1, 4]:
                     for sliced_mode in [False, True]:
