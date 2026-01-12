8b1650ccac92d275829b6233c7cd3f195417058d
Anatoly Starostin <anatoli.starostin@gmail.com>
2026-01-12 17:28:07 +0200

    andn lut

--- src/spiky/lut/ANDNLUTLayer.py
+++ src/spiky/lut/ANDNLUTLayer.py
@@ -144,7 +144,7 @@ class ANDNLUTLayer(LUTLayerBasic):
         )
         self._output_shape = (lut_shape[0], lut_shape[1] * n_lut_channels)
         self._inhibition_layer = Grid2DInhibitionLayer(
-            self._output_shape,
+            self._andn_layer.output_shape(),
             output_kernel_shape
         )
         self._andn_layer.set_descendant_andn_layer(self._inhibition_layer)
