3d73410953cbd2ae6b4da5f1db23d8586a65ef71
Anatoly Starostin <anatoli.starostin@gmail.com>
2026-01-27 16:06:59 +0200

    Preparing first release

--- spiky_cuda/andn/aux/andn_runtime_I.cu
+++
@@ -1,2 +0,0 @@
-#define INTEGERS_INSTEAD_OF_FLOATS
-#include "../andn_runtime.cu"
--- spiky_cuda/spiky_py.cpp
+++ spiky_cuda/spiky_py.cpp
@@ -4,11 +4,9 @@
 namespace py = pybind11;
 
 void PB_SPNetDataManagerF(py::module& m);
-void PB_ANDNDataManagerF(py::module& m);
 void PB_LUTDataManagerF(py::module& m);
 #ifdef BUILD_INTEGERS_VERSION
 void PB_SPNetDataManagerI(py::module& m);
-void PB_ANDNDataManagerI(py::module& m);
 void PB_LUTDataManagerI(py::module& m);
 #endif
 void PB_SynapseGrowthLowLevelEngine(py::module& m);
@@ -17,11 +15,9 @@ void PB_DenseToSparseConverter(py::module& m);
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.doc() = "Spiky Module";
     PB_SPNetDataManagerF(m);
-    PB_ANDNDataManagerF(m);
     PB_LUTDataManagerF(m);
     #ifdef BUILD_INTEGERS_VERSION
     PB_SPNetDataManagerI(m);
-    PB_ANDNDataManagerI(m);
     PB_LUTDataManagerI(m);
     #endif
     PB_SynapseGrowthLowLevelEngine(m);
