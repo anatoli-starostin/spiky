4db9519d6a77cd37ce9a1a1bb3a7f38726a3d292
Anatoly Starostin <anatoli.starostin@gmail.com>
2026-03-10 20:26:36 +0200

    lutorch in progress

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
             "spiky.util",
             "spiky.lut_fused",
             "spiky.lutorch",
-            "spiky.lutorch.tests",
             "spiky.spnet"
         ],
     )
